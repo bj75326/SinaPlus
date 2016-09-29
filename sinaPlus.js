b0b47c3428e422c8d86f0efd4339a8776ad158e1
Bill Ji <superice_gy@163.com>
2016-09-29 16:13:01 +0800

    version2 completed

--- sinaPlus.js
+++ sinaPlus.js
@@ -58,12 +58,31 @@ var Bin = {
         }
     },
 
+    /*
     styleChange: function (element, style, interval) {
         return new Promise(function (resolve, reject) {
             element.setAttribute("style", style);
             setTimeout(function () {
                 resolve(null);
             }, interval);
+        });
+    }, */
+
+    styleChange: function (elements, style, interval){
+        return new Promise(function (resolve, reject){
+            if(Object.prototype.toString.call(elements).slice(8, -1) === "Array"){
+                elements.forEach(function(element){
+                    element.setAttribute("style", style);
+                });
+                setTimeout(function(){
+                    resolve(null);
+                }, interval);
+            }else{
+                elements.setAttribute("style", style);
+                setTimeout(function(){
+                    resolve(null);
+                }, interval);
+            }
         });
     },
 
@@ -111,6 +130,13 @@ Bin.ready(function () {
     var popup = document.querySelector(".popup");
     var Icons = document.querySelectorAll(".popup .icons .icon a");
     var close = document.querySelector(".popup .exit");
+    var close2 = document.querySelector(".popup .exit2");
+    var backBtn = document.querySelector(".popup .exit2 .fa:nth-child(1)");
+    var closeBtn = document.querySelector(".popup .exit2 .fa:nth-child(2)");
+    var more = document.querySelector(".icons .group:nth-child(1) .icon:nth-child(6)");
+    var IconWrapper = document.querySelector(".popup .icons");
+
+    var width = window.innerWidth;
 
     var popupFn = function () {
         //overlayer.setAttribute("style", "display: block");
@@ -122,17 +148,17 @@ Bin.ready(function () {
         }).then(function () {
             return Bin.styleChange(closeIcon, "transform: rotate(0deg)", 0);
         }).then(function () {
-            return Bin.styleChange(Icons[0], "transform: translate(0, 0)", 50);
+            return Bin.styleChange([Icons[0],Icons[6]], "transform: translate(0, 0)", 50);
         }).then(function () {
-            return Bin.styleChange(Icons[1], "transform: translate(0, 0)", 50);
+            return Bin.styleChange([Icons[1],Icons[7]], "transform: translate(0, 0)", 50);
         }).then(function () {
-            return Bin.styleChange(Icons[2], "transform: translate(0, 0)", 50);
+            return Bin.styleChange([Icons[2],Icons[8]], "transform: translate(0, 0)", 50);
         }).then(function () {
-            return Bin.styleChange(Icons[3], "transform: translate(0, 0)", 50);
+            return Bin.styleChange([Icons[3],Icons[9]], "transform: translate(0, 0)", 50);
         }).then(function () {
-            return Bin.styleChange(Icons[4], "transform: translate(0, 0)", 50);
+            return Bin.styleChange([Icons[4],Icons[10]], "transform: translate(0, 0)", 50);
         }).then(function () {
-            return Bin.styleChange(Icons[5], "transform: translate(0, 0)", 50);
+            return Bin.styleChange([Icons[5],Icons[11]], "transform: translate(0, 0)", 50);
         });
     };
 
@@ -140,25 +166,74 @@ Bin.ready(function () {
         document.body.setAttribute("style", ""); //
 
         Bin.styleChange(closeIcon, "", 0).then(function () {
-            return Bin.styleChange(Icons[5], "", 50);
+            return Bin.styleChange([Icons[5], Icons[11]], "", 50);
         }).then(function () {
-            return Bin.styleChange(Icons[4], "", 50);
+            return Bin.styleChange([Icons[4], Icons[10]], "", 50);
         }).then(function () {
-            return Bin.styleChange(Icons[3], "", 50);
+            return Bin.styleChange([Icons[3], Icons[9]], "", 50);
         }).then(function () {
-            return Bin.styleChange(Icons[2], "", 50);
+            return Bin.styleChange([Icons[2], Icons[8]], "", 50);
         }).then(function () {
-            return Bin.styleChange(Icons[1], "", 50);
+            return Bin.styleChange([Icons[1], Icons[7]], "", 50);
         }).then(function () {
-            return Bin.styleChange(Icons[0], "", 600);
+            return Bin.styleChange([Icons[0], Icons[6]], "", 600);
         }).then(function () {
             return Bin.styleChange(popup, "", 0);
         }).then(function () {
             return Bin.styleChange(overlayer, "", 0);
+        }).then(function(){
+            return Bin.styleChange(IconWrapper, "", 0);
+        }).then(function(){
+            close.setAttribute("style", "");
+            close2.setAttribute("style", "");
+        });
+    };
+
+    var swipeFn = function(){
+        Bin.styleChange(IconWrapper, "transform: translate(-" + width + "px, 0", 0).then(function(){
+            close.setAttribute("style", "display: none");
+            close2.setAttribute("style", "display: flex");
+        });
+    };
+
+    var swipeBackFn = function(){
+        Bin.styleChange(IconWrapper, "", 0).then(function(){
+            close.setAttribute("style", "");
+            close2.setAttribute("style", "");
         });
     };
 
     Bin.on(plus, "touchstart", popupFn, false);
     Bin.on(close, "touchstart", popupCloseFn, false);
-    Bin.on(overlayer, "touchstart", popupCloseFn, false);
+    //Bin.on(overlayer, "touchstart", popupCloseFn, false);
+    Bin.on(more, "touchstart", swipeFn, false);
+    Bin.on(closeBtn, "touchstart", popupCloseFn, false);
+    Bin.on(backBtn, "touchstart", swipeBackFn, false);
+
+    //fix overlayer gesture issue
+    var startX, startY;
+
+    Bin.on(overlayer, "touchstart", function(ev){
+        var event = ev || window.event;
+        console.log("touchstart");
+        console.log(event);
+        event.preventDefault();
+        var touch = event.touches[0];
+        console.log(touch);
+        startX = touch.pageX;
+        startY = touch.pageY;
+    }, false);
+
+    Bin.on(overlayer, "touchmove", function(ev){
+        var event = ev || window.event;
+        console.log("touchmove");
+        console.log(event);
+        event.preventDefault();
+        var touch = event.touches[0];
+        var deltaX = touch.pageX - startX;
+        var deltaY = touch.pageY - startY;
+        if(Math.abs(deltaX) > Math.abs(deltaY) && deltaX < 0){
+            popupCloseFn();
+        }
+    }, false);
 });
